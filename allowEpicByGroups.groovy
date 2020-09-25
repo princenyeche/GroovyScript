e8d0df28584ee05c6662105d72d466d77dfb8185
Prince <hakwumerunyeche@gmail.com>
2020-09-25 16:42:03 +0800

    Update allowEpicByGroups.groovy

--- allowEpicByGroups.groovy
+++ allowEpicByGroups.groovy
@@ -11,15 +11,17 @@ def allIssueTypes = ComponentAccessor.constantsManager.allIssueTypeObjects
 def user = ComponentAccessor.jiraAuthenticationContext.loggedInUser
 def issueTypeField = getFieldById(ISSUE_TYPE)
 String err = "You are not allowed to create an Epic"
+String group1 = "jira-administrators"
+String group2 = "gym-group"
 
-def remoteUsersGroup = projectGroupManager.getGroupsForUser(user.username)
+def remoteUsersGroup = projectGroupManager.getGroupNamesForUser(user)
 def availableIssueTypes = []
 
-if ("jira-administrator" in remoteUsersGroup) {
+if (remoteUsersGroup.contains(group1)) {
     availableIssueTypes.addAll(allIssueTypes.findAll { it.name in ["Bug", "Task", "Story", "Theme", "Initiative"] })
     log.warn(err)
 }
-if ("gym-group" in remoteUsersGroup) {
+if (remoteUsersGroup.contains(group2)) {
     availableIssueTypes.addAll(allIssueTypes.findAll { it.name in ["Bug", "Task", "Story", "Theme", "Initiative", "Epic"] })
 }
 
